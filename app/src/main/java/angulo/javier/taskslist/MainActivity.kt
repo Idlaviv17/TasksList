41508533886ee75056c59ba446aee0ed392055c7
Idlaviv <jjavier1708@gmail.com>
2024-03-05 16:04:09 -0700

    Initial commit

---
+++ app/src/main/java/angulo/javier/taskslist/MainActivity.kt
@@ -0,0 +1,11 @@
+package angulo.javier.taskslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
