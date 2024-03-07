fe98b0452bd3a80cebea551ea7597e80592a8f1f
Idlaviv <jjavier1708@gmail.com>
2024-03-07 10:11:27 -0700

    Add persistence to existing functionality

--- app/src/main/java/angulo/javier/taskslist/MainActivity.kt
+++ app/src/main/java/angulo/javier/taskslist/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
     lateinit var et_tarea: EditText
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var listview_tareas: ListView
     lateinit var lista_tareas: ArrayList<String>
     lateinit var adaptor: ArrayAdapter<String>
+    lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         lista_tareas = ArrayList()
 
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "tareas-db"
+        ).allowMainThreadQueries().build()
+
+        cargarTareas()
+
         adaptor = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista_tareas)
         listview_tareas.adapter = adaptor
 
         btn_agregar.setOnClickListener {
-            var tarea = et_tarea.text.toString()
+            var tarea_str = et_tarea.text.toString()
 
-            if(!tarea.isNullOrEmpty()) {
-                lista_tareas.add(tarea)
+            if(!tarea_str.isNullOrEmpty()) {
+                var tarea = Tarea(desc = tarea_str)
+                db.tareaDao().agregarTarea(tarea)
+                lista_tareas.add(tarea_str)
                 adaptor.notifyDataSetChanged()
                 et_tarea.setText("")
             } else {
@@ -42,8 +53,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         listview_tareas.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            var tarea_desc = lista_tareas[position]
+
+            var tarea = db.tareaDao().getTarea(tarea_desc)
+
+            db.tareaDao().eliminarTarea(tarea)
+
             lista_tareas.removeAt(position)
             adaptor.notifyDataSetChanged()
+        }
+    }
+
+    private fun cargarTareas() {
+        var lista_db = db.tareaDao().obtenerTareas()
+        for(tarea in lista_db) {
+            lista_tareas.add(tarea.desc)
         }
     }
 }
